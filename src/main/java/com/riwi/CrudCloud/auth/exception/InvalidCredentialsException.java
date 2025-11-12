af95a154d90f8c2552ed3cf7c9ecc31c10d931d5
Jteban1 <sepesteban@gmail.com>
2025-11-12 01:01:13 -0500

    refactor: deprecate old exception classes in favor of new architecture

--- src/main/java/com/riwi/CrudCloud/auth/exception/InvalidCredentialsException.java
+++ src/main/java/com/riwi/CrudCloud/auth/exception/InvalidCredentialsException.java
@@ -1,12 +1,26 @@
 package com.riwi.CrudCloud.auth.exception;
 
-public class InvalidCredentialsException extends AuthException {
+/**
+ * Exception thrown when user provides invalid login credentials.
+ * Extends BadRequestException (HTTP 400 Bad Request)
+ * 
+ * Used for:
+ * - Invalid email/password combinations
+ * - Authentication failures
+ * 
+ * Note: In a more strict implementation, this could use HTTP 401 Unauthorized,
+ * but BadRequest is used here for failed validation of credentials.
+ * 
+ * @deprecated Consider throwing BadRequestException directly with context
+ */
+@Deprecated(since = "1.1", forRemoval = true)
+public class InvalidCredentialsException extends BadRequestException {
 
     public InvalidCredentialsException() {
-        super("Invalid email or password", "INVALID_CREDENTIALS");
+        super("Invalid email or password");
     }
 
     public InvalidCredentialsException(String message) {
-        super(message, "INVALID_CREDENTIALS");
+        super(message);
     }
 }
