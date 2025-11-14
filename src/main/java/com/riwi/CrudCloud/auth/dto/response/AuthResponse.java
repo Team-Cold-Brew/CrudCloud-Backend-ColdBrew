7f77a58ee1477f78d5e1a35b9aaa85ba13e91aea
Jteban1 <sepesteban@gmail.com>
2025-11-13 19:05:54 -0500

    refactor(auth): consolidate response DTOs with Lombok annotations

--- src/main/java/com/riwi/CrudCloud/auth/dto/response/AuthResponse.java
+++ src/main/java/com/riwi/CrudCloud/auth/dto/response/AuthResponse.java
@@ -1,30 +1,18 @@
 package com.riwi.CrudCloud.auth.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AuthResponse {
 
     private String token;
+    private String tokenType;
     private UserResponse user;
-
-    // Constructor
-    public AuthResponse(String token, UserResponse user) {
-        this.token = token;
-        this.user = user;
-    }
-
-    // Getters and Setters
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public UserResponse getUser() {
-        return user;
-    }
-
-    public void setUser(UserResponse user) {
-        this.user = user;
-    }
+    private String message;
 }
