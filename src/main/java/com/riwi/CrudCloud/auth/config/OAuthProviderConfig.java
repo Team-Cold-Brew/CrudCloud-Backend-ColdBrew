8ee53bfaa2f4ab6bad236762c35d6a99d707b615
Jteban1 <sepesteban@gmail.com>
2025-11-13 19:05:55 -0500

    refactor(config): add SuppressWarnings annotation to OAuthProviderConfig

--- src/main/java/com/riwi/CrudCloud/auth/config/OAuthProviderConfig.java
+++ src/main/java/com/riwi/CrudCloud/auth/config/OAuthProviderConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @Getter
+@SuppressWarnings("unused")
 public class OAuthProviderConfig {
 
     // Google OAuth Configuration
